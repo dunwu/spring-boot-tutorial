5fee83df301856755a7359e5dd5ce46046d4e397
Zhang Peng <forbreak@163.com>
2018-11-02 14:18:36 +0800

    添加 dozer 示例

---
+++ codes/spring-boot-bean-dozer/src/main/java/io/github/dunwu/springboot/dozer/Info.java
@@ -0,0 +1,13 @@
+package io.github.dunwu.springboot.dozer;
+
+public class Info {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ codes/spring-boot-bean-dozer/src/main/java/io/github/dunwu/springboot/dozer/UserDO.java
@@ -0,0 +1,31 @@
+package io.github.dunwu.springboot.dozer;
+
+/**
+ * @author Zhang Peng
+ * @date 2018-11-02
+ */
+public class UserDO {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[UserDO] id = %d, name = %s", id, name);
+    }
+}
