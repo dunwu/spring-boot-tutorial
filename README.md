06b6cf0d9d9adf4b6f624a9dc12d0f66f9e0a8ec
Zhang Peng <forbreak@163.com>
2018-12-12 15:51:50 +0800

    example: spring-boot-web-consuming-rest

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 - [spring-boot-log-logback](codes/spring-boot-log-logback)
 - [spring-boot-properties](codes/spring-boot-properties)
 - [spring-boot-schedule](codes/spring-boot-schedule)
+- [spring-boot-web-consuming-rest](codes/spring-boot-web-consuming-rest)
 - [spring-boot-web-helloworld](codes/spring-boot-web-helloworld)
 
---
+++ codes/spring-boot-web-consuming-rest/src/main/java/io/github/dunwu/springboot/springboot/ConsumingRestApplication.java
@@ -0,0 +1,34 @@
+package io.github.dunwu.springboot.springboot;
+
+import io.github.dunwu.springboot.springboot.bean.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApplication.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) {
+        return args -> {
+            Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
+}
