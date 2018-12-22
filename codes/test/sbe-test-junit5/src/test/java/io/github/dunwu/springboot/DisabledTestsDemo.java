f3285af5e99e4e7cacd82f23b9f21ea14afcebb1
Zhang Peng <forbreak@163.com>
2018-12-22 20:06:56 +0800

    example: sbe-test-junit5

---
+++ codes/test/sbe-test-junit5/src/test/java/io/github/dunwu/springboot/DisabledTestsDemo.java
@@ -0,0 +1,16 @@
+package io.github.dunwu.springboot;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class DisabledTestsDemo {
+
+    @Disabled
+    @Test
+    void testWillBeSkipped() {
+    }
+
+    @Test
+    void testWillBeExecuted() {
+    }
+}
---
+++ codes/test/sbe-test-junit5/src/test/java/io/github/dunwu/springboot/SystemPropertyTestsDemo.java
@@ -0,0 +1,23 @@
+package io.github.dunwu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+
+/**
+ * @author Zhang Peng
+ * @date 2018-12-22
+ */
+public class SystemPropertyTestsDemo {
+    @Test
+    @EnabledIfSystemProperty(named = "os.arch", matches = ".*64.*")
+    void onlyOn64BitArchitectures() {
+        // ...
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "ci-server", matches = "true")
+    void notOnCiServer() {
+        // ...
+    }
+}
