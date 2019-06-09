0662f818c24e55ca91dfffd615781fc0bdd51516
Zhang Peng <forbreak@163.com>
2019-06-09 09:16:41 +0800

    update spring-security 示例

---
+++ codes/security/sbe-security-web/src/main/java/io/github/dunwu/springboot/MvcConfig.java
@@ -0,0 +1,23 @@
+package io.github.dunwu.springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * MVC 配置
+ * @author <a href="mailto:forbreak@163.com">Zhang Peng</a>
+ * @since 2019-06-09
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
--- codes/security/sbe-security-web/src/main/java/io/github/dunwu/springboot/SampleWebSecureApplication.java
+++ codes/security/sbe-security-web/src/main/java/io/github/dunwu/springboot/SampleWebSecureApplication.java
@@ -1,19 +1,9 @@
 package io.github.dunwu.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Date;
-import java.util.Map;
 
 @SpringBootApplication
 @Controller
@@ -21,43 +11,6 @@ public class SampleWebSecureApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(SampleWebSecureApplication.class).run(args);
-    }
-
-    @GetMapping("/")
-    public String home(Map<String, Object> model) {
-        model.put("message", "Hello World");
-        model.put("title", "Hello Home");
-        model.put("date", new Date());
-        return "home";
-    }
-
-    @RequestMapping("/foo")
-    public String foo() {
-        throw new RuntimeException("Expected exception in controller");
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
-    }
-
-
-    @Configuration
-    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            // @formatter:off
-			http.authorizeRequests()
-					.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-					.anyRequest().fullyAuthenticated()
-					.and()
-				.formLogin().loginPage("/login").failureUrl("/login?error").permitAll()
-					.and()
-				.logout().permitAll();
-			// @formatter:on
-        }
-
     }
 
 }
