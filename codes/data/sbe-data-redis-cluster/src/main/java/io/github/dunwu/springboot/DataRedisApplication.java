611f14c40f25223ff7cd5f6035f23cb761747f4d
Zhang Peng <forbreak@163.com>
2019-08-22 11:06:36 +0800

    Spring Boot 访问 Redis Cluster 示例

---
+++ codes/data/sbe-data-redis-cluster/src/main/java/io/github/dunwu/springboot/DataRedisApplication.java
@@ -0,0 +1,11 @@
+package io.github.dunwu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataRedisApplication.class, args);
+    }
+}
