50d6db9eddb2a0072f0f07c40c2c028a8dbc7b83
Zhang Peng <forbreak@163.com>
2019-10-09 23:47:01 +0800

    验证码示例

---
+++ examples/sbe-security/sbe-security-kaptcha/src/main/java/io/github/dunwu/springboot/security/controller/KaptchaController.java
@@ -0,0 +1,43 @@
+package io.github.dunwu.springboot.security.controller;
+
+import io.github.dunwu.springboot.security.util.KaptchaUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.time.LocalDateTime;
+
+/**
+ * @author <a href="mailto:forbreak@163.com">Zhang Peng</a>
+ * @since 2019/10/9
+ */
+@RestController
+@RequestMapping("code")
+public class KaptchaController {
+
+	@GetMapping(value = "image")
+	public void authImage(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+		response.setHeader("Pragma", "No-cache");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setDateHeader("Expires", 0);
+		response.setContentType("image/jpeg");
+		// 生成随机字串
+		KaptchaUtil.Kaptcha kaptcha = KaptchaUtil.create(60);
+		// 存入会话session
+		HttpSession session = request.getSession(true);
+		// 删除以前的
+		session.removeAttribute("code");
+		session.removeAttribute("expireTime");
+		session.setAttribute("code", kaptcha.getCode());
+		session.setAttribute("expireTime", kaptcha.getExpireTime());
+		OutputStream out = response.getOutputStream();
+		KaptchaUtil.toOutputStream(kaptcha, out);
+	}
+
+}
---
+++ examples/sbe-security/sbe-security-kaptcha/src/main/java/io/github/dunwu/springboot/security/handler/CustomAuthenticationSucessHandler.java
@@ -0,0 +1,25 @@
+package io.github.dunwu.springboot.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSucessHandler implements AuthenticationSuccessHandler {
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request,
+			HttpServletResponse response, Authentication authentication)
+			throws IOException {
+		redirectStrategy.sendRedirect(request, response, "/index");
+	}
+
+}
