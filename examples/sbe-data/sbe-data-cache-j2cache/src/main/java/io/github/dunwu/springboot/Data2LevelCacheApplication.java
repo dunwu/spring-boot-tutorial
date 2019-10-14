45517e0ce9bfa74e8f50251f1797e03104e48989
Zhang Peng <forbreak@163.com>
2019-10-14 21:20:58 +0800

    更新缓存示例

---
+++ examples/sbe-data/sbe-data-cache-j2cache/src/main/java/io/github/dunwu/springboot/Data2LevelCacheApplication.java
@@ -0,0 +1,19 @@
+package io.github.dunwu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author <a href="mailto:forbreak@163.com">Zhang Peng</a>
+ * @since 2019/10/14
+ */
+@SpringBootApplication
+public class Data2LevelCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data2LevelCacheApplication.class, args);
+	}
+
+}
