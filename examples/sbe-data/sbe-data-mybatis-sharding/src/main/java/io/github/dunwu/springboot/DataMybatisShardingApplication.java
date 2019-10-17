20997b2c5878bc78a768dcfeb7fb5aefa8d42d1a
Zhang Peng <forbreak@163.com>
2019-10-17 18:58:17 +0800

    Spring Boot + Mybatis Plus + Sharding-Jdbc 示例

---
+++ examples/sbe-data/sbe-data-mybatis-sharding/src/main/java/io/github/dunwu/springboot/DataMybatisShardingApplication.java
@@ -0,0 +1,43 @@
+package io.github.dunwu.springboot;
+
+import io.github.dunwu.springboot.mapper.UserMapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:forbreak@163.com">Zhang Peng</a>
+ * @since 2019/10/16
+ */
+@SpringBootApplication
+@MapperScan("io.github.dunwu.springboot.mapper")
+public class DataMybatisShardingApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(DataMybatisShardingApplication.class);
+
+	private final UserMapper userMapper;
+
+	public DataMybatisShardingApplication(UserMapper userMapper) {
+		this.userMapper = userMapper;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMybatisShardingApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		if (userMapper == null) {
+			log.error("连接数据源失败");
+			return;
+		} else {
+			log.info("连接数据源成功");
+		}
+
+	}
+
+}
---
+++ examples/sbe-data/sbe-data-mybatis-sharding/src/main/resources/sql/clear.sql
@@ -0,0 +1,11 @@
+DROP DATABASE `master_databases`;
+DROP DATABASE `slave_databases_0`;
+DROP DATABASE `slave_databases_1`;
+
+DROP DATABASE `sharding_databases_0`;
+DROP DATABASE `sharding_databases_1`;
+
+DROP DATABASE `sharding_databases_tables_0`;
+DROP DATABASE `sharding_databases_tables_1`;
+
+DROP DATABASE `sharding_tables`;
