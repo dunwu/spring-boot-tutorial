f3537a8c93912d9bc69c882ff284f17144840843
Zhang Peng <forbreak@163.com>
2019-11-18 21:47:11 +0800

    更新安全相关示例

--- spring-boot-security-remember-me/src/main/resources/sql/data.sql
+++
@@ -1,5 +0,0 @@
----------------------------------------------
--- 运行本项目的初始化 DML 脚本
----------------------------------------------
-
-INSERT INTO user (username, password) VALUES ('admin', '$2a$10$Y9uV9YjFuNlATDGz5MeTZeuo8LbebbpP6jRgtZYQcgiCZRlf8rJYG') ;
