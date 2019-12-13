8fd03120e6c2b5675a62d085e5a7f31cc4ea631a
Zhang Peng <forbreak@163.com>
2019-12-13 11:59:22 +0800

    更新 Junit5 示例

--- spring-boot-test-junit5/src/main/java/io/github/dunwu/springboot/HelloController.java
+++ spring-boot-test-junit5/src/main/java/io/github/dunwu/springboot/HelloController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	private final HelloService helloService;
+
+	public HelloController(HelloService helloService) {
+		this.helloService = helloService;
+	}
+
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello World";
+		return helloService.hello();
 	}
 
 }
